b4917390b23b6cfbda396c671d14065fd679854f
leonard <l.hackel@tu-berlin.de>
2024-06-18 10:15:56 +0200

    go back to fake data

--- scripts/train_BENv2.py
+++ scripts/train_BENv2.py
@@ -115,7 +115,7 @@ def main(
     data_dirs = resolve_data_dir(data_dirs, allow_mock=True)
     print(f"Using data directories for {hostname}")
 
-    use_mock_data = False
+    use_mock_data = True
     if use_mock_data:
         dm = MockDataModule(
             dims=(channels, img_size, img_size),
