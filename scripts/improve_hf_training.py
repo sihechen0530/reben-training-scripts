8e61dc339c899c52901f83816fad867ca8e09a74
leonard <l.hackel@tu-berlin.de>
2024-06-20 17:15:37 +0200

    improved layout of improve script to better refelct the individual steps

--- scripts/improve_hf_training.py
+++ scripts/improve_hf_training.py
@@ -1,40 +1,18 @@
 import lightning.pytorch as pl
 import torch
 import typer
+from configilm.ConfigILM import ILMConfiguration
 from configilm.extra.BENv2_utils import resolve_data_dir
 from configilm.extra.DataModules.BENv2_DataModule import BENv2DataModule
-from torchvision import transforms
 from lightning.pytorch.loggers import WandbLogger
+from torchvision import transforms
 
 from ben_publication.BENv2ImageClassifier import BENv2ImageEncoder
 from scripts.load_BENv2_pretrained_from_hf import download_and_evaluate_model
 from scripts.train_BENv2 import _get_benv2_dir_dict
 
 
-def main(
-        model_name: str = typer.Option("BIFOLD-BigEarthNetv2-0/BENv2-resnet50-42-s2-v0.1.1", help="Model name"),
-        seed: int = typer.Option(42, help="Random seed"),
-        lr: float = typer.Option(0.001, help="Learning rate"),
-        epochs: int = typer.Option(100, help="Number of epochs"),
-        bs: int = typer.Option(32, help="Batch size"),
-        drop_rate: float = typer.Option(0.375, help="Dropout rate"),
-        drop_path_rate: float = typer.Option(0.0, help="Drop path rate"),
-        warmup: int = typer.Option(-1, help="Warmup steps, set to -1 for automatic calculation"),
-        workers: int = typer.Option(8, help="Number of workers"),
-        use_wandb: bool = typer.Option(False, help="Use wandb for logging"),
-        test_run: bool = typer.Option(True, help="Run training and eval with fewer epochs and batches")
-):
-    upload_hf_entity = None  # "BIFOLD-BigEarthNetv2-0"
-    pl.seed_everything(seed, workers=True)
-    torch.set_float32_matmul_precision("medium")
-
-    # load the model from Huggingface Hub and evaluate to get a baseline
-    compare_results = download_and_evaluate_model(model_name=model_name, limit_test_batches=5 if test_run else None)
-    compare_metric = compare_results["AveragePrecision"]["macro"]
-
-    # train the model with the given hyperparameters based on the config of the downloaded model
-    config = BENv2ImageEncoder.from_pretrained(model_name).config
-
+def get_arch_version_bandconfig(model_name: str, config: ILMConfiguration):
     architecture = model_name.split("/")[-1].split("-")[1]
     assert architecture == config.timm_model_name, f"Model name {architecture} does not match config {config.timm_model_name}"
     version = model_name.split("/")[-1].split("-")[-1]
@@ -47,6 +25,24 @@ def main(
         assert config.channels == 12, f"Bandconfig {bandconfig} does not match config {config.channels}"
     else:
         raise ValueError(f"Unknown band configuration {bandconfig}")
+    return architecture, version, bandconfig
+
+
+def train_new_model(
+        config: ILMConfiguration,
+        comparison_model_name: str,
+        lr: float,
+        epochs: int,
+        bs: int,
+        drop_rate: float,
+        drop_path_rate: float,
+        warmup: int,
+        workers: int,
+        use_wandb: bool,
+        test_run: bool,
+        seed: int,
+):
+    architecture, version, bandconfig = get_arch_version_bandconfig(comparison_model_name, config)
 
     model = BENv2ImageEncoder(config, lr=lr, warmup=warmup)
 
@@ -126,6 +122,48 @@ def main(
     dm.train_transform = train_transforms
 
     trainer.fit(model, dm)
+    return model, dm, trainer
+
+
+def main(
+        model_name: str = typer.Option("BIFOLD-BigEarthNetv2-0/BENv2-resnet50-42-s2-v0.1.1", help="Model name"),
+        seed: int = typer.Option(42, help="Random seed"),
+        lr: float = typer.Option(0.001, help="Learning rate"),
+        epochs: int = typer.Option(100, help="Number of epochs"),
+        bs: int = typer.Option(32, help="Batch size"),
+        drop_rate: float = typer.Option(0.375, help="Dropout rate"),
+        drop_path_rate: float = typer.Option(0.0, help="Drop path rate"),
+        warmup: int = typer.Option(-1, help="Warmup steps, set to -1 for automatic calculation"),
+        workers: int = typer.Option(8, help="Number of workers"),
+        use_wandb: bool = typer.Option(False, help="Use wandb for logging"),
+        test_run: bool = typer.Option(True, help="Run training and eval with fewer epochs and batches")
+):
+    upload_hf_entity = None  # "BIFOLD-BigEarthNetv2-0"
+    pl.seed_everything(seed, workers=True)
+    torch.set_float32_matmul_precision("medium")
+
+    # load the model from Huggingface Hub and evaluate to get a baseline
+    compare_results = download_and_evaluate_model(model_name=model_name, limit_test_batches=5 if test_run else None)
+    compare_metric = compare_results["AveragePrecision"]["macro"]
+
+    # train the model with the given hyperparameters based on the config of the downloaded model
+    config = BENv2ImageEncoder.from_pretrained(model_name).config
+
+    model, dm, trainer = train_new_model(
+        config=config,
+        comparison_model_name=model_name,
+        lr=lr,
+        epochs=epochs,
+        bs=bs,
+        drop_rate=drop_rate,
+        drop_path_rate=drop_path_rate,
+        warmup=warmup,
+        workers=workers,
+        use_wandb=use_wandb,
+        test_run=test_run,
+        seed=seed,
+    )
+
     new_results = trainer.test(model, datamodule=dm, ckpt_path="best")
     new_metric = new_results[0]["test/MultilabelAveragePrecision_macro"]
 
@@ -134,8 +172,8 @@ def main(
     print(f"New metric: {new_metric:.4f}")
     if new_metric > compare_metric:
         print(f"New model improved the compare metric by {new_metric - compare_metric:.4f}")
-
         print("=== Uploading model to Huggingface Hub ===")
+        architecture, version, bandconfig = get_arch_version_bandconfig(model_name, config)
         new_model_name = f"BENv2-{architecture}-{seed}-{bandconfig}-{version}"
         assert model_name == new_model_name, f"Model name {model_name} does not match new model name {new_model_name}"
         if upload_hf_entity:
