e7b87250b94c07ecbabc6b260b34eda4a09d21a9
liuguoyi <liu.guoy@northeasern.edu.cn>
2025-11-11 16:34:59 -0800

    update name

--- scripts/train_resnet.sh
+++ scripts/train_resnet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=train_all_no_rgb
+#SBATCH --job-name=train_s2_no_rgb
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=8:00:00
-#SBATCH --output=logs/train_all_no_rgb_%j.out
-#SBATCH --error=logs/train_all_no_rgb_%j.err
+#SBATCH --output=logs/train_s2_no_rgb_%j.out
+#SBATCH --error=logs/train_s2_no_rgb_%j.err
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=8
 
@@ -19,7 +19,7 @@ conda activate /home/chen.sihe1/.conda/envs/dinov3/
 
 # 打印作业信息
 echo "========================================="
-echo "作业名称: train_s2"
+echo "作业名称: train_s2_no_rgb"
 echo "作业ID: $SLURM_JOB_ID"
 echo "节点: $SLURM_NODELIST"
 echo "GPU: $CUDA_VISIBLE_DEVICES"
@@ -34,7 +34,7 @@ python train_BigEarthNetv2_0.py \
     --bandconfig=all_no_rgb \
     --lr=0.0001 \
     --bs=512 \
-    --resume-from /home/liu.guoy/reben-training-scripts/scripts/checkpoints/resnet101-42-11-val_mAP_macro-0.55.ckpt
+    --resume-from /home/liu.guoy/reben-training-scripts/scripts/checkpoints/resnet101-42-9-val_mAP_macro-0.61.ckpt
 
 # 打印完成信息
 echo ""
